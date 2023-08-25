f2ef4e7fc0720ede5ee127dd949f6477554fe61a
Joe Attardi <jattardi@gmail.com>
2023-08-25 17:04:45 -0400

    linkify book titles

--- src/routes/books/BookCard.svelte
+++ src/routes/books/BookCard.svelte
@@ -11,7 +11,7 @@
 <div class="flex flex-col sm:flex-row space-x-4 my-6">
   <a {href}><img class="w-[40vw] md:w-[20vw] max-w-none" src={coverUrl} alt={`Cover for ${title}`} /></a>
   <div class="flex flex-col flex-grow">
-    <h3 class="text-3xl">{title}</h3>
+    <h3 class="text-3xl"><a href={href}>{title}</a></h3>
     <div>{publisher}, {year}</div>
     <div class="my-4"><slot /></div>
     <div class="my-4"><CallToAction {href}>Get <em>{title}</em></CallToAction></div>
